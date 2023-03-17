eb47bb752a9a21aafd62609c57e50b122fc44e5e
yiyaowen <wenyiyao_fort@163.com>
2023-03-17 16:11:35 +0800

    Remove template files

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'D14Engine'
+copyright = '2023, D14Studio'
+author = 'yiyaowen'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = ['myst_parser']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
