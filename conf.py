4d64425fc70aad98a0027aebdd87c179136ce7c8
yiyaowen <wenyiyao_fort@163.com>
2023-05-08 19:19:22 +0800

    Adjust hierarchy

---
+++ conf.py
@@ -0,0 +1,22 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'D14Engine'
+author = 'yiyaowen'
+copyright = '2023, yiyaowen'
+version = 'beta'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
